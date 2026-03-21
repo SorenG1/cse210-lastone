358679a69ce6145b6ecafca5b130cabb20d09ccd
SorenG1 <gom23015@byui.edu>
2026-03-21 16:19:45 -0600

    Finished the Learning Activity late

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Soren Gomez", "Weird things");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+
+        MathAssignment m1 = new MathAssignment("Anna Ross", "Division", "15.5", "10-11");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment w1 = new WritingAssignment("Logan Cowley", "Multiplication", "THE MATH WORLD");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+        Console.WriteLine();
+
     }
 }
